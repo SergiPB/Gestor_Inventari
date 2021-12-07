7829056c4257aa55d3900a05e47e5e91212b0077
SergiPB <94857445+SergiPB@users.noreply.github.com>
2021-12-07 15:06:09 +0100

    Add files via upload

--- Gestor_inventari.java
+++ Gestor_inventari.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -12,12 +15,23 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Gestor_inventari {
 
     static Scanner teclat = new Scanner(System.in);
     static Connection connexioBD;
+    static String nom_empresa = "Mobles DAM";
+    static String direccio_emp = "Tarrega";
+    static int telefon = 646163674;
+
+    static FileWriter fitcher = null;
+    static BufferedWriter bf = null;
+    static PrintWriter escritor = null;
+
+    static String[] proveidors = new String[3];
+    static int[] productesCom = new int[3];
 
     public static void main(String[] args) throws IOException {
 
@@ -56,23 +70,23 @@ public class Gestor_inventari {
             int opcio = teclat.nextInt();
 
             switch (opcio) {
-            case 1:
-                gestorProductes();
-                break;
-            case 2:
-                actualitzarStok();
-                break;
-            case 3:
-                prepararComanda();
-                break;
-            case 4:
-                analitzarComandes();
-                break;
-            case 5:
-                sortir = true;
-                break;
-            default:
-                System.out.println("Opció no valida");
+                case 1:
+                    gestorProductes();
+                    break;
+                case 2:
+                    actualitzarStok();
+                    break;
+                case 3:
+                    prepararComanda();
+                    break;
+                case 4:
+                    analitzarComandes();
+                    break;
+                case 5:
+                    sortir = true;
+                    break;
+                default:
+                    System.out.println("Opció no valida");
 
             }
         } while (!sortir);
@@ -92,23 +106,23 @@ public class Gestor_inventari {
         teclat.nextLine();
 
         switch (opcio) {
-        case 1:
-            llistarTotsProductes();
-            break;
-        case 2:
-            consutlaProducte();
-            break;
-        case 3:
-            altaProducte();
-            break;
-        case 4:
-            modificacioProducete();
-            break;
-        case 5:
-            baixaProducte();
-            break;
-        default:
-            System.out.println("Opció no valida");
+            case 1:
+                llistarTotsProductes();
+                break;
+            case 2:
+                consutlaProducte();
+                break;
+            case 3:
+                altaProducte();
+                break;
+            case 4:
+                modificacioProducete();
+                break;
+            case 5:
+                baixaProducte();
+                break;
+            default:
+                System.out.println("Opció no valida");
 
         }
 
@@ -262,7 +276,6 @@ public class Gestor_inventari {
             System.out.println("codi_cat: " + rs.getInt("codi_cat"));
         }
 
-        teclat.nextLine();
     }
 
     static void baixaProducte() throws SQLException {
@@ -336,11 +349,138 @@ public class Gestor_inventari {
 
     }
 
-    static void prepararComanda() {
+    static void prepararComanda() throws SQLException, IOException {
+        int productes = 0;
+        int prov = 0;
+        int prod = 0;
 
+        System.out.println("GENERA COMANDES");
+
+        String consulta = "select productes.codi, estoc, proveeix.nif, proveidors.nom, proveidors.localitat, proveidors.telefon from productes join proveeix on productes.codi = proveeix.codi join proveidors on proveeix.nif = proveidors.nif  where estoc < 20 order by proveeix.nif;";
+
+        PreparedStatement ps = connexioBD.prepareStatement(consulta);
+
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+
+            String actproveidor = rs.getString("nif");
+            String nomproveidor = rs.getString("nom");
+            String localitatProv = rs.getString("localitat");
+            int telefonProv = rs.getInt("telefon");
+
+            crearFitxer(nomproveidor, localitatProv, telefonProv, prov);
+
+            do {
+
+                if (!actproveidor.equals(rs.getString("nif"))) {
+                    System.out.println("Ha canviat el proveidor");
+
+                    actproveidor = rs.getString("nif");
+                    nomproveidor = rs.getString("nom");
+
+                    escritor.close();
+
+                    prov++;
+
+                    crearFitxer(nomproveidor, localitatProv, telefonProv, prov);
+
+                    prod++;
+                    productes = 0;
+
+                }
+
+                System.out.print("codi: " + rs.getInt("codi") + "  ");
+                System.out.print("estoc: " + (500 - rs.getInt("estoc")) + "  ");
+                System.out.println("NIF: " + rs.getString("nif"));
+
+                escritor.println("\t" + rs.getInt("codi") + "\t \t" + (500 - rs.getInt("estoc")));
+
+                productes++;
+                productesCom[prod] = productes;
+
+            } while (rs.next());
+
+            escritor.close();
+
+        }
+    }
+
+    static void crearFitxer(String nomproveidor, String localitat, int telefonProv, int prov) throws IOException {
+
+        // Crear el fitcher
+        fitcher = new FileWriter("files/COMANDES/" + nomproveidor + "_" + LocalDate.now() + ".txt");
+        bf = new BufferedWriter(fitcher);
+        escritor = new PrintWriter(bf);
+
+        escritor.println("Empresa:");
+        escritor.println(nom_empresa + " " + direccio_emp);
+        escritor.println("Telefon: " + telefon);
+
+        escritor.println("\nProveidor:");
+        escritor.println(nomproveidor + " " + localitat);
+        escritor.println("Telefon: " + telefonProv);
+
+        escritor.println("\nCodi Producte\tEstoc Solisitat");
+
+        proveidors[prov] = nomproveidor;
     }
 
     static void analitzarComandes() {
+        llistatProductes();
+        mesSollisitat();
+        menysSollisitat();
+        mitjanaSollisitat();
+
+    }
+
+    static void llistatProductes() {
+        System.out.println("Llistat de Productes demanats");
+        for (int i = 0; i < proveidors.length; i++) {
+            System.out.println(proveidors[i] + ": " + productesCom[i]);
+        }
+    }
+
+    static void mesSollisitat() {
+        System.out.println("\nProveidor mes sol·lisitat");
+        int max = productesCom[0];
+        int imax = 0;
+
+        for (int i = 0; i < productesCom.length; i++) {
+            if (productesCom[i] > max) {
+                max = productesCom[i];
+                imax = i;
+            }
+        }
+        System.out.println(proveidors[imax] + " se sol·lisiten : " + max);
+
+    }
+
+    static void menysSollisitat() {
+        System.out.println("\nProveidor menys sol·lisitat");
+        int min = productesCom[0];
+        int imin = 0;
+
+        for (int i = 0; i < productesCom.length; i++) {
+            if (productesCom[i] < min) {
+                min = productesCom[i];
+                imin = i;
+            }
+        }
+        System.out.println(proveidors[imin] + " se sol·lisiten : " + min);
+    }
+
+    static void mitjanaSollisitat() {
+        System.out.println("\nMitjana de productes sol·lisitats");
+
+        double mitjana = 0, suma = 0;
+
+        for (int i = 0; i < productesCom.length; i++) {
+            suma += productesCom[i];
+
+        }
+        mitjana = suma / productesCom.length;
+        System.out.println("La mitjana es " + mitjana + "\n");
 
     }
 
